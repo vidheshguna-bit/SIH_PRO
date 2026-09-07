2b7c6dffe2b5016d9715a097deaa0c4d325ef0ba
Kabbilan <kabbilan5608@gmail.com>
2026-09-07 12:31:50 +0530

    Load SmartMetrology v4 frontend enhancements

--- static/bootstrap.js
+++ static/bootstrap.js
@@ -1,2 +1,4 @@
+document.write('<link rel="stylesheet" href="/static/v4.css">');
 document.write('<script src="https://sih-pro-legal-metrology.onrender.com/static/script.js"><\/script>');
 document.write('<script src="/static/immersive.js"><\/script>');
+document.write('<script src="/static/v4_ui.js"><\/script>');
